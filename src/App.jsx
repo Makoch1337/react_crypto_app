559a4b7804769e8ec812683444e6e5f3479ca41a
Makoch1337 <lashok13370228@gmail.com>
2024-08-08 17:02:18 +0300

    sider_done-Header_0,7

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import React from 'react';
-import { Layout } from 'antd';
-import { calc } from 'antd/es/theme/internal';
-import AppHeader from './Components/Layout/AppHeader';
-import AppSider from './Components/Layout/AppSider';
-import AppContent from './Components/Layout/AppContent';
+import { CryptoContextProvider } from './context/crypto-context';
+import AppLayout from './Components/Layout/AppLayout';
 
 
 export default function App() {
   return (
-    <Layout>
-      <AppHeader />
-      <Layout>
-        <AppSider />
-        <AppContent />
-      </Layout>
-    </Layout>
+    <CryptoContextProvider>
+      <AppLayout />
+    </CryptoContextProvider>
   )
 }
--- src/Components/Layout/AppSider.jsx
+++ src/Components/Layout/AppSider.jsx
@@ -1,56 +1,57 @@
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
-import { Card, Col, Row, Statistic, Layout, List, Typography } from 'antd';
-import { useEffect, useState } from 'react';
-
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-];
+import { Card, Col, Row, Statistic, Layout, List, Typography, Tag } from 'antd';
+import { capitalizer } from '../../utils'
+import { useContext } from 'react';
+import CryptoContext from '../../context/crypto-context';
 
 const siderStyle = {
     padding: '1rem'
 };
 
 export default function AppSider() {
-    const [loading, setLoading] = useState(false)
-
-
-    useEffect(() => {
-        async function preload() {
-            setLoading(true)
-            const { result } = await fakeFetchCrypto()
-            const assets = await fetchAssets()
-        }
-    }, [])
-
+    const { assets } = useContext(CryptoContext)
 
     return (
         <Layout.Sider width="25%" style={siderStyle}>
-            <Card style={{ marginBottom: '1rem' }}>
-                <Statistic
-                    title="Active"
-                    value={11.28}
-                    precision={2}
-                    valueStyle={{
-                        color: '#3f8600',
-                    }}
-                    prefix={<ArrowUpOutlined />}
-                    suffix="%"
-                />
-                <List
-                    size='small'
-                    dataSource={data}
-                    renderItem={(item) => (
-                        <List.Item>
-                            <Typography.Text mark>[ITEM]</Typography.Text> {item}
-                        </List.Item>
-                    )}
-                />
-            </Card>
-            <Card>
+            {assets.map(asset => (
+                <Card key={asset.id} style={{ marginBottom: '1rem' }}>
+                    <Statistic
+                        title={capitalizer(asset.id)}
+                        value={asset.totalAmount}
+                        precision={2}
+                        valueStyle={{
+                            color: asset.grow ? '#3f8600' : '#cf1322',
+                        }}
+                        prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+                        suffix="$"
+                    />
+                    <List
+                        size='small'
+                        dataSource={[
+                            { title: 'Total Profit', value: asset.totalProfit, withTag: true },
+                            { title: 'Asset Amount', value: asset.amount, isPlayn: true },
+                            // { title: 'Differences', value: asset.growPercent },
+                        ]}
+                        renderItem={(item) => (
+                            <List.Item>
+                                <span>{item.title}</span>
+                                <span>
+                                    {item.withTag && (
+                                        <Tag color={asset.grow ? 'green' : 'red'}>
+                                            {asset.growPercent}%
+                                        </Tag>
+                                    )}
+                                    {item.isPlayn && item.value}
+                                    {!item.isPlayn && (
+                                        <Typography.Text type={asset.grow ? 'success' : 'danger'} >{item.value.toFixed(2)}$</Typography.Text>
+                                    )}
+                                </span>
+                            </List.Item>
+                        )}
+                    />
+                </Card>
+            ))}
+            {/* <Card>
                 <Statistic
                     title="Idle"
                     value={9.3}
@@ -61,7 +62,7 @@ export default function AppSider() {
                     prefix={<ArrowDownOutlined />}
                     suffix="%"
                 />
-            </Card>
+            </Card> */}
         </Layout.Sider>
     )
 }
